179b4f20780cc6db8fae54b93aeb11cdd30f0d4e
saikiran1111 <kiran.skm.sai5@gmail.com>
2019-09-06 10:41:38 -0700

    git started

--- mysite/loginform/views.py
+++ mysite/loginform/views.py
@@ -1,58 +1,88 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .forms import UserRegisterForm
 import requests
+from requests import Request, Session
+from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
+import json
 
-def home(request):
-	count =User.objects.count()
-	return render(request,'home.html',{
-		'count':count
-		})
+def homes(request):
+	
+	return render(request,'homes.html')
 
 
 
 
-def base(request):
+def index(request):
     data = {}
     data["crypto_data"] = get_crypto_data()
-    return render(request, "base.html", data)
+    return render(request, "index.html", data)
 
 
 def get_crypto_data():
-    api_url = "https://api.coinmarketcap.com/v1/ticker/?limit=8"
+	url = 'https://poloniex.com/public?command=returnTradeHistory&currencyPair=BTC_ETH'
+	parameters ={
+		'id':'1'
+	}
+	headers = {
+	  'Accepts': 'application/json',
+	  'X-CMC_PRO_API_KEY': 'PE7QD910-N2TDEPAP-LZILGG72-E3ICHKJW',
+	}
 
-    try:
-        data = requests.get(api_url).json()
-    except Exception as e:
-        print(e)
-        data = dict()
+	session = Session()
+	session.headers.update(headers)
 
-    return data
+	try:
+	  response = session.get(url, params=parameters)
+	  data = json.loads(response.text)
+	  return data
+	except (ConnectionError, Timeout, TooManyRedirects) as e:
+	  print(e)
 
-def bot(request):
+def main(request):
 
 	
-	return render(request,'bot.html')
+	return render(request,'main.html')
 
-def exchange(request):
+def home(request):
 	
-	return render(request,'exchange.html')
+	return render(request,'home.html')
 
 
 def logo(request):
 	
 	return render(request,'logo.html')
 
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Your account has been created! You are now able to log in')
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'users/register.html', {'form': form})
 
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
+"""
 def signup(request):
 	if request.method =='POST':
 		form=UserCreationForm(request.POST)
 		if form.is_valid():
 			form.save()
-			return redirect('base')
+			return redirect('home')
 	else:
 		form=UserCreationForm()
-	return render(request,'registration/sign.html',{
+	return render(request,'registration/signup.html',{
 		'form':form
 		})
+"""
--- mysite/urls.py
+++ mysite/urls.py
@@ -4,20 +4,23 @@ from django.urls import path,include
 from django.conf import settings
 from mysite.loginform import views 
 from django.conf.urls.static import static
-
+from django.contrib.auth import views as auth_views
 urlpatterns = [
-	#path('',views.home,name="home"),
+	path('homes',views.homes,name="homes"),
 
-	path('bot',views.bot,name="bot"),
+	path('',views.main,name="main"),
 	path('finance',views.logo,name="logo"),
 	
-	path('',views.base,name="base"),
-	path('exchange',views.exchange,name="exchange"),
+	path('home',views.home,name="home"),
+	path('index',views.index,name="index"),
 	
-	path('sign/',views.signup,name="sign"),
+	#path('signup',views.signup,name="signup"),
 	path('^oauth/', include('social_django.urls', namespace='social')),
-    path('accounts/',include('django.contrib.auth.urls')),
-    
+    #path('accounts/  ',include('django.contrib.auth.urls')),
+    path('register', views.register, name='register'),
+    path('profile/', views.profile, name='profile'),
+    path('loginpage', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('admin/', admin.site.urls),
 ]
 
